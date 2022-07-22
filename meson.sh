f85d9f8cd29d1325014c60411a780d75a1a71ab4
Farzad <fz.karimnejad@gmail.com>
2022-07-22 20:30:45 +0430

    Update meson.sh

--- meson.sh
+++ meson.sh
@@ -5,7 +5,14 @@ echo "======== Install update and upgrade =============="
 sudo apt-get update -y && sudo apt-get install wget -y
 sleep 1
 echo "======== download & install meson =============="
-wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.18/meson_cdn-linux-amd64.tar.gz' && tar -zxf meson_cdn-linux-amd64.tar.gz && rm -f meson_cdn-linux-amd64.tar.gz && cd ./meson_cdn-linux-amd64 && sudo ./service install meson_cdn
+wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.18/meson_cdn-linux-amd64.tar.gz' 
+tar -zxf meson_cdn-linux-amd64.tar.gz
+sleep 1
+rm -f meson_cdn-linux-amd64.tar.gz
+sleep 1
+cd ./meson_cdn-linux-amd64
+sleep 1
+sudo ./service install meson_cdn
 sleep 1
 echo "======== config meson =============="
 sudo ./meson_cdn config set --token=ahckkeketftrzghnnntoezty --https_port=443 --cache.size=100

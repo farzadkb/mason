35eecaf426dade3412dc051442bf9d3cc18963fa
Farzad <fz.karimnejad@gmail.com>
2022-07-22 18:54:59 +0300

    add install file

---
+++ meson.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "=================================================="
+sleep 1
+echo "======== Install update and upgrade =============="
+sudo apt-get update -y && sudo apt-get install wget -y
+sleep 1
+echo "======== download & install meson =============="
+wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.18/meson_cdn-linux-amd64.tar.gz' && tar -zxf meson_cdn-linux-amd64.tar.gz && rm -f meson_cdn-linux-amd64.tar.gz && cd ./meson_cdn-linux-amd64 && sudo ./service install meson_cdn
+sleep 1
+echo "======== config meson =============="
+sudo ./meson_cdn config set --token=ahckkeketftrzghnnntoezty --https_port=443 --cache.size=100
+sleep 1
+echo "======== start meson =============="
+sudo ./service start meson_cdn
+sleep 1
+echo "======== install and start completed =============="

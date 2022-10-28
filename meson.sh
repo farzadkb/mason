7f2623e6c5584852d6a938e6a0bc9e243bfdc5cf
Farzad <fz.karimnejad@gmail.com>
2022-10-29 00:46:04 +0330

    Update meson.sh

--- meson.sh
+++ meson.sh
@@ -6,9 +6,9 @@ sudo apt-get update -y && sudo apt-get install wget -y
 sleep 1
 echo "======== download & install meson =============="
 sudo apt install wget
-sleep1
+sleep 1
 sudo ufw allow 443/tcp
-sleep1
+sleep 1
 wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.18/meson_cdn-linux-amd64.tar.gz'
 tar -zxf meson_cdn-linux-amd64.tar.gz
 rm -f meson_cdn-linux-amd64.tar.gz
